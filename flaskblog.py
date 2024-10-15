b9b5b8ab676b09c52b057b74845ee792fc9e7721
Ericazzzzz <ericalow2001@gmail.com>
2024-10-16 00:27:23 +0100

    Creating templates

--- flaskblog.py
+++ flaskblog.py
@@ -1,16 +1,34 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
+posts =[
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date': 'April 20, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date': 'April 21, 2018'
+    },
+]
+
 
 @app.route("/")
 @app.route("/home")
 def hello():
-    return "<h1>Home Page</h1>"
+    return render_template('home.html', posts=posts) #passing the dummy variable (latter) to a variable named posts(former)
+
+
 def hello():
     return "<h1>Home Page</h1>"
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</h1>"
+    return render_template('about.html', title ='About')
 
 
 
