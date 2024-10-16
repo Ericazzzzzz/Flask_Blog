c1d6a27fc058950ff87a6f619a33121aff719d61
Ericazzzzzz <ericalow2001@gmail.com>
2024-10-16 23:23:51 +0100

    Forums

--- flaskblog.py
+++ flaskblog.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'b79bf25643fd10ff819f36875969a09e'
 
 posts =[
     {
@@ -23,13 +26,20 @@ def hello():
     return render_template('home.html', posts=posts) #passing the dummy variable (latter) to a variable named posts(former)
 
 
-def hello():
-    return "<h1>Home Page</h1>"
-
 @app.route("/about")
 def about():
     return render_template('about.html', title ='About')
 
+
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == '__main__':      #__main_- only on python, else == file name
